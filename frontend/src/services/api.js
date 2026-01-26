2e7f9e5ca7d9e8fbca736a20b7260a919b7d7bc9
Shivam Gupta <sg210590@gmail.com>
2026-01-26 06:22:31 +0000

    Fix API baseURL to use VITE_API_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,38 @@
+// // src/services/api.js
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:5000/api"
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("access_token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+
+
+
 // src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api"
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
