ff43dde4cd8a46d0652cdabca11afe11e152078a
Shivam Gupta <sg210590@gmail.com>
2026-03-07 16:38:32 +0000

    migrate sqlite to supabase

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -1,3 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Shivam@0837@db.ajzfmzjiicjcafqyjtqy.supabase.co:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
